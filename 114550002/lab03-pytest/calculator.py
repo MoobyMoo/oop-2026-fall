cd81b769fe8d60850c2d0c5c0f2e9d2c7b0f91e2
bolingh <aaaron08813@gmail.com>
2026-09-21 16:52:42 +0800

    update calculator.py and test_calculator.py

--- 114550002/lab03-pytest/calculator.py
+++ 114550002/lab03-pytest/calculator.py
@@ -2,4 +2,6 @@ def add(a, b):
     return a + b
 
 def devide(a, b):
+    if b == 0:
+        raise ValueError('Error: divide by 0')
     return a / b
--- 114550002/lab03-pytest/test_calculator.py
+++ 114550002/lab03-pytest/test_calculator.py
@@ -6,5 +6,5 @@ def test_add_two_number():
     assert add(2, 3) == 5
 
 def test_devide_two_number():
-    with pytest.raises(ZeroDivisionError):
+    with pytest.raises(ValueError):
         devide(5, 0)
