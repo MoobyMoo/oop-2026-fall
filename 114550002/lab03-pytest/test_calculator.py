46bdc35db155bfe8ed3239d74434598224d84ed2
bolingh <aaaron08813@gmail.com>
2026-09-21 16:37:36 +0800

    calculator.py and pytest

---
+++ 114550002/lab03-pytest/test_calculator.py
@@ -0,0 +1,10 @@
+import pytest
+from calculator import add
+from calculator import devide
+
+def test_add_two_number():
+    assert add(2, 3) == 5
+
+def test_devide_two_number():
+    with pytest.raises(ZeroDivisionError):
+        devide(5, 0)
